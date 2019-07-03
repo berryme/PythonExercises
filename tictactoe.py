ff32182530ecc1412acd91886031df070cb163e1
berryme <berryme@hotmail.com>
2019-07-02 20:35:21 -0400

    Milestone Project 1

--- tictactoe.py
+++ tictactoe.py
@@ -3,19 +3,26 @@ import random
 
 
 def display_board(board):
+    clear_output()
     print(board[1] + '|' + board[2] + '|' + board[3])
-    print("_____")
+    print( "_____")
     print(board[4] + '|' + board[5] + '|' + board[6])
-    print("_____")
+    print( "_____")
     print(board[7] + '|' + board[8] + '|' + board[9])
 
 
 def player_input():
     player1 = ''
+    player2 = ''
     while player1.upper() not in ['X', 'O']:
         player1 = input("Please pick a marker 'X' or 'O'")
 
-    return player1
+     if player1 =='X':
+         player2 == 'O'
+     else:
+         player2 == 'X'
+
+    return (player1,player2)
 
 
 def place_marker(board, marker, position):
