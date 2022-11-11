367ce77d90a5c7febf05f6e632f4eb1d7f716325
Matt Berry <berryme@users.noreply.github.com>
2022-11-11 08:46:16 -0500

    Updating Master From Class Branch (#1)
    * Unit test start
    
    * laptop
    
    * class req and notes
    
    * class req and notes
    
    * more classwork
    
    * test exceptions
    
    * 44. Function Practice Exercises -
    Overview
    
    * exercises
    
    * 44. Function Practice Exercises -
    Overview finished except just for fun
    
    * solutions for ex 1
    
    * Up to Homework 51
    
    * 53. Functions and Methods Homework - Solutions
    
    * Milestone Project 1
    
    * Milestone Project 1
    
    * Delete vcs.xml
    
    * Delete R_User_Library.xml
    
    * Update requirements.txt
    
    Got rid of PARSO package in requirements.txt
    
    * OOP Section 8
    
    * OOP Section 8 reformat
    
    * Finished Section 10
    
    * Generators section
    
    * yaml
    
    * config files
    
    * begin Section 15
    
    * Advanced sections
    
    * Bump notebook from 6.0.0 to 6.1.5
    
    Bumps [notebook](https://github.com/jupyter/jupyterhub) from 6.0.0 to 6.1.5.
    - [Release notes](https://github.com/jupyter/jupyterhub/releases)
    - [Changelog](https://github.com/jupyterhub/jupyterhub/blob/master/CHECKLIST-Release.md)
    - [Commits](https://github.com/jupyter/jupyterhub/commits)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump bleach from 3.1.0 to 3.3.0
    
    Bumps [bleach](https://github.com/mozilla/bleach) from 3.1.0 to 3.3.0.
    - [Release notes](https://github.com/mozilla/bleach/releases)
    - [Changelog](https://github.com/mozilla/bleach/blob/master/CHANGES)
    - [Commits](https://github.com/mozilla/bleach/compare/v3.1.0...v3.3.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump jinja2 from 2.10.1 to 2.11.3
    
    Bumps [jinja2](https://github.com/pallets/jinja) from 2.10.1 to 2.11.3.
    - [Release notes](https://github.com/pallets/jinja/releases)
    - [Changelog](https://github.com/pallets/jinja/blob/master/CHANGES.rst)
    - [Commits](https://github.com/pallets/jinja/compare/2.10.1...2.11.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * update depends in requirments.txt
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ berryexception.py
@@ -0,0 +1,15 @@
+import enum
+
+
+class BerryException(Exception):
+    keys = enum.Enum('keys', 'ONE TWO UNKNOWN')
+    __errors = {keys.ONE: 'Error One'}
+
+    def __init__(self, key):
+
+        if isinstance(key, BerryException.keys):
+            self.key = key
+            self.message = BerryException.__errors[key]
+        else:
+            self.key = BerryException.keys.UNKNOWN
+            self.message = key
---
+++ testexception.py
@@ -0,0 +1,36 @@
+import unittest
+
+from berryexception import BerryException
+
+
+def raise_ex(key):
+    raise BerryException(key)
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_something(self):
+        self.assertEqual(True, 1 == 1)
+
+    def test(self):
+        try:
+            raise_ex(BerryException.keys.ONE)
+        except BerryException as e:
+            print(e)
+            print(e.key)
+            print(e.message)
+
+            self.assertEqual('Error One', e.message)
+
+    def test2(self):
+        try:
+            raise_ex('Generic message')
+        except BerryException as e:
+            print(e)
+            print(e.key)
+            print(e.message)
+            self.assertEqual('Generic message', e.message)
+
+
+if __name__ == '__main__':
+    unittest.main()
